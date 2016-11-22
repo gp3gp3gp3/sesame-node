0bc27ab9b20a235d91b84c837142ca095d11c409
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-11-22 16:20:30 -0500

    Add working promise for iframe

--- public/js/iframe.js
+++ public/js/iframe.js
@@ -1,1 +1,9 @@
-console.log('hello')
+const loadNode = () => {
+  return new Promise ((resolve, reject) => {
+    frames[0].onload = e => resolve(e)
+  })
+}
+
+loadNode().then(res => {
+  document.body.children[0].appendChild(res.target.body.lastElementChild)
+})
