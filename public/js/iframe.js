a84b9adfa1929bcab40a0fb81b9aa1496897378f
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-11-22 16:37:52 -0500

    Add error catching in promise

--- public/js/iframe.js
+++ public/js/iframe.js
@@ -1,9 +1,12 @@
 const loadNode = () => {
   return new Promise ((resolve, reject) => {
     frames[0].onload = e => resolve(e)
+    frames[0].onerror = e => reject(e)
   })
 }
 
-loadNode().then(res => {
-  document.body.children[0].appendChild(res.target.body.lastElementChild)
-})
+loadNode()
+  .then(res => {
+    document.body.children[0].appendChild(res.target.body.lastElementChild)
+  })
+  .catch(err => console.error('iframe failed to load', err))
