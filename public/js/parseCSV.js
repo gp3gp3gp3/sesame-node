d29e7ffb1e70d86850975e6f0ff682a5885fccc5
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-11-22 22:00:40 -0500

    add working csv parse

--- public/js/parseCSV.js
+++ public/js/parseCSV.js
@@ -1,6 +1,28 @@
-$.get('files/attachment2.csv', data => {
-  const csvData = Papa.parse(data, {header: true})
+const drawCSV = user => {
+  let htmlStr = ''
+  htmlStr += `<h3>${user.first_name} ${user.last_name}</h3>`
+  htmlStr += `<div>`
+  htmlStr += `<p><strong>Email</strong>: <a href="mailto:${user.email}">${user.email}</a></p>`
+  htmlStr += `<p><strong>Company Name</strong>: ${user.company_name}</p>`
+  htmlStr += `<p><strong>Address</strong>: ${user.address}</p>`
+  htmlStr += `<p><strong>City</strong>: ${user.city}</p>`
+  htmlStr += `<p><strong>County</strong>: ${user.county}</p>`
+  htmlStr += `<p><strong>Zip</strong>: ${user.postal}</p>`
+  htmlStr += `<p><strong>Phone number</strong>: ${user.phone1}</p>`
+  htmlStr += `<p><strong>Secondary Phone number</strong>: ${user.phone2}</p>`
+  htmlStr += `<p><strong>Website</strong>: <a target="_blank" href="${user.web}">${user.web}</a></p>`
+  htmlStr += `</div>`
+  return htmlStr
+}
 
-  console.log(csvData)
-  $('.content').append(csvData.data.map(e => `<p>${e.first_name}</p>`))
+$.get('files/attachment2.csv', data => {
+  const csvData = Papa.parse(data, {
+    worker: true,
+    header: true,
+    complete: () => {
+      $('#loading').remove()
+      $('#accordion').accordion({collapsible: true})
+    },
+    step: row => $('#accordion').append(drawCSV(row.data[0]))
+  })
 })
